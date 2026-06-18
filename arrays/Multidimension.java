fe597eb775a6d1f095bf0fa08b3b0031fd9578da
Aizen sama <156284808+Mahesh8899p@users.noreply.github.com>
2026-06-18 06:55:16 +0530

    arraychanges

--- arrays/Multidimension.java
+++ arrays/Multidimension.java
@@ -1,5 +1,5 @@
 package arrays;
-
+import java.util.Scanner;
 public class Multidimension {
     public static void main(String[] args) {
         // int[][]arr = new int[3][];
@@ -17,11 +17,22 @@ public class Multidimension {
         //             v        v        v
         //          [1,2,3]  [4,5,6]  [7,8,9]   <- 3 separate inner int[] blocks
     int[][] arr2D = {
-        {},
-        {},
-        {}
+        {1,2,3}, //0th index
+        {4,5},   //1st index
+        {6,7,8,9} //2nd index 
+    };
+    Scanner in = new Scanner(System.in);
+
+    //input of multidimensional array
+    int[][] arr1 = new int[3][];
+    for(int row =0;row < arr1.length;row++){
+        for(int col = 0;col<arr1[row].length;col++){
+            System.out.println(arr1[row][col]);
+        }
     }
-    
+
+
+
     
     }
 
